acd195dc6e390db5a98f18d94de49644e35d72b3
liyankai <775166744@qq.com>
2016-11-07 22:30:22 +0800

    提交基本思路

---
+++ src/stack/infix2postfix.c
@@ -0,0 +1,15 @@
+/*
+ * infix2postfix.c
+ *
+ *  Created on: Nov 7, 2016
+ *      Author: angellee
+ */
+
+#include <stdio.h>
+
+//从stdin 读入一行
+	//参考链接
+		//http://stackoverflow.com/questions/12252103/how-to-read-a-line-from-stdin-blocking-until-the-newline-is-found
+		//http://man7.org/linux/man-pages/man3/getline.3.html
+//替换 -X 为 (0-X)
+//解析行，把infix转换为postfix
